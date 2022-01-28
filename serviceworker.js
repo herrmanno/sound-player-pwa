ec76fd91d86a5a9bf98f35ce342c115fb73e0baf
Oliver Herrmann <herrmann@id-on.de>
2022-01-28 12:12:47 +0100

    initial commit

---
+++ serviceworker.js
@@ -0,0 +1,32 @@
+const cacheName = "v3"
+const sounds = [
+    "gong",
+    "sword",
+    "glass"
+]
+
+self.addEventListener("install", function (e) {
+    e.waitUntil(
+        caches.open(cacheName).then(function (cache) {
+            return cache.addAll(["index.html", ...sounds.map(s => `sounds/${s}.mp3`)])
+        })
+    )
+})
+
+self.addEventListener("fetch", function (event) {
+    if (event.request.url === "http://cache/keys") {
+        event.respondWith(
+            caches.open(cacheName)
+            .then(c => c.keys())
+            .then(ks => {
+                return new Response(JSON.stringify(ks.map(r => r.url)), {status: 200})
+            })
+        )
+    } else {
+        event.respondWith(
+            caches.match(event.request).then(function (response) {
+                return response || fetch(event.request)
+            })
+        )
+    }
+})
